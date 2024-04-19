8c484e1522b11a90b6b25685d06b653bd2203e90
BabaGanoush624 <youssef.amr7488@gmail.com>
2024-04-19 21:39:42 +0200

    added explanatory comments on the mapUtils file

--- src/services/mapUtils.js
+++ src/services/mapUtils.js
@@ -1,7 +1,9 @@
+// importing the required registries
 import { apiRegistry, actionsRegistry } from "@penta-b/ma-lib";
-
+//vectorLayer declaration
 let VL = null;
 
+// Checking the if the vector layer already exists to clear all the features on it(which won't happen until after the first click)
 const validateVL = async (options) => {
     if (!VL) {
         const [VectorLayer] = await apiRegistry.getApis(["VectorLayer"]);
@@ -22,25 +24,27 @@ const generateStyle = async (styleOptions) => {
     let style;
     style = new Style(
         new Fill(styleOptions.color),
-        new Stroke("#000000", 1, null),
+        new Stroke("#000000", 1, null), //takes Color(string), width, lineDash
         new Circle(
-            new Fill(styleOptions.color),
-            new Stroke("#000000", 1, null),
-            7
+            new Fill(styleOptions.color), //fill takes only one parameter => Color(string)
+            new Stroke("#000000", 1, null), // stroke
+            7 //readius ???confilct with the buffer radius???
         )
     );
     return style;
 };
 
+//converting the geoJsonFeature(buffer) into olFeature that can be drawn on the map
 const generateFeature = async (GEOJSONFeature) => {
     const [Feature] = await apiRegistry.getApis(["Feature"]);
     return new Feature({ ...GEOJSONFeature });
 };
 
+// drawing the GEOJSONFeature (buffer) on the map
 export const drawFeautres = async (GEOJSONFeatures, options) => {
     await validateVL(options.vectorLayerOptions);
     const features = await Promise.all(GEOJSONFeatures.map(generateFeature));
     const style = await generateStyle(options.styleOptions);
-    VL.setStyle(style);
-    VL.addFeatures(features);
+    VL.setStyle(style); // is this style applied to the whole of vector layer and not to the specified feature only
+    VL.addFeatures(features); // adding the feature generated to the vectorLayer
 };
